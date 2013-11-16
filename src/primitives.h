04742b054276a3af1081b066e8bc7f22392d64fe
smart0eddie <smart0eddie@gmail.com>
2013-11-16 03:16:57 -0800

    set up
    change to triangle only
    flat array, no obj
    vector operation

--- src/primitives.h
+++ src/primitives.h
@@ -7,164 +7,49 @@
 // - Light
 // - Color
 // - Camera 
-// - MatrixStack
-// - Matrix 
+// - Triangle
+// - AABB Tree?
 //****************************************************
 
 #ifndef LIGHTS_H
 #define LIGHTS_H
 
 #include <cstdlib>
-#include <vector>
-#include <iostream>
-#include <fstream>
 #include <cmath>
 
 using namespace std;
 
+/* Type */
+typedef float Vector3[4]; // [x, y, z, dummy]
+typedef const float constVector3[4];
+typedef float Color[4]; // [r, g, b, dummy]
+typedef const float constColor[4];
+typedef float Ray[8]; // [Vector3[origin], Vector3[direction]]
+typedef const float constRay[8];
+typedef float Light[12]; // [Vector3[source] , Color[color], isDirectional, dummy, dummy, dummy]
+typedef const float constLight[12];
+typedef float Brdf[16]; // [Color[kd], Color[ks], Color[ke], kr, sp, dummy, dummy]
+typedef const float constBrdf[16];
+typedef float Triangle[16]; // [Vector3[pt1], Vector3[pt2], Vector3[pt3], Vector3[normal]]
+typedef const float constTriangle[16];
 
-//****************************************************
-// CLASS DECLARATIONS AND CONSTRUCTORS 
-//****************************************************
-class Vector; 
-class Point{ 
-public:
-    Point() {};
-    Point(double, double, double); 
+/* utility function */
+//vector
+void vector3Add(Vector3 dst, constVector3 v1, constVector3 v2);
+void vector3Sub(Vector3 dst, constVector3 v1, constVector3 v2);
+void vector3Scale(Vector3 dst, constVector3 v1, const float scalar);
+float vector3Dot(constVector3 v1, constVector3 v2);
+void vector3Cross(Vector3 dst, constVector3 v1, constVector3 v2);
+void vector3Normalize(Vector3 dst, constVector3 v1);
+void vector3Copy(Vector3 dst, constVector3 src);
 
-    double x, y, z;
-    Vector subtract(Point); 
-    Point operator + (Vector); //position + direction = position 
-    Point operator + (Point); // point addition; shortcut for camera's ray formula
-    Point operator * (double); 
-    Vector operator - (Point);  
-    
-    
-    
-};
+//triangle
+bool intersectTriangle();
 
-    
-    
-class Vector{
- public:
-    Vector() {};  
-    Vector(double, double, double);
-    Vector(Point); 
-    Vector(Point, Point); 
-    
-    double dx, dy, dz;
-    double mag;
-    
-    Vector normalize();
-    double dotProduct(Vector);
-    Vector negative();
-    Vector crossProduct(Vector);
-    
-    //allow for scalar * and vector +
-    Vector operator * (double);
-    Vector operator + (Vector); 
-    Vector operator - (Vector); 
-    
-    
-};
+//ray
 
+//light
 
+//aabb tree
 
-
-class Ray{
-public:
-    Ray() {}; 
-    Ray(double, double, double, double, double, double);
-    Ray(Point, Point); 
-    Ray(Point, Vector);
-
-    Vector direction; 
-    Point origin; 
-   
-    
-};
-
-
-/*
- *  Color class keeps track of r g b values, which are [0, 1]
- */ 
- class Color{
-     public:
-        Color() {};
-        Color(double, double, double);
-        
-        double r, g, b;
-        
-        Color operator + (Color);  
-        Color operator * (Color); 
-        Color operator * (double); 
-        void operator += (Color); 
-        
-        Color clone();
- };
-
-/*
- *  Light class describes a light source that extends in all directions with rgb value from point x y z
- */ 
-
-class Light {
-  public:
-    Light() {}; 
-    Light (double, double, double, double, double, double, bool); 
-
-    void initPos(double, double, double);
-    void initRGB(double, double, double); 
-    Color color; 
-    Point source; 
-    double x, y, z, r, g, b;
-	bool directional;
-
-};
-
-class Matrix{
-    public:
-		double m[4][4]; 
-    Matrix();
-    
-    // Instantiator shortcut for translation, scaling, or rotating (depends upon input char)
-    Matrix(char, double, double, double, double); 
-    
-    Matrix operator * (Matrix); 
-    Point operator * (Point);
-    Vector operator * (Vector);
-    Ray operator * (Ray);
-    
-    Matrix invert();
-    void debug();
-    Matrix clone();
-    
-    Vector vectorTimesM(Vector); 
-};
-
-class MatrixStack{
-    public:
-    MatrixStack(); 
-    
-    vector<Matrix> stack;
-    vector<Matrix> stackT;
-    vector<Matrix> stackS;
-    vector<Matrix> stackR;
-    Matrix product;
-    Matrix productT; // product of all the T matrices
-    Matrix productS;
-    Matrix productR;
-    
-    void push(); 
-    void pop();
-    void addTransform(Matrix, char);
-     
-};
-
-
-
-
-
-
-
- 
 #endif
